55302373de3dbc5f9bb36b1e7a1997fd6c946211
John Lim <jnsg.lim0@gmail.com>
2023-06-29 17:05:30 -0700

    Created new files codewars18.js, codewars19.js, codewars20.js

---
+++ codewars19.js
@@ -0,0 +1,27 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+function abbrevName(name){
+    let abbrev = []
+    name.toUpperCase().split(' ').forEach(x => abbrev.push(x[0]))
+    return `${abbrev[0]}.${abbrev[1]}`
+}
+
+console.log(abbrevName("Sam Harris"))
+console.log(abbrevName("Patrick Feenan"))
+console.log(abbrevName("Evan Cole"))
+console.log(abbrevName("P Favuzzi"))
+console.log(abbrevName("David Mendieta"))
+
+// function abbrevName(name){
+
+//     return name.split(' ').map(i => i[0].toUpperCase()).join('.')
+
+// }
