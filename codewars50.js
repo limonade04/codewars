02fb8bc739602ecdb66793f014002bff2f50c498
John Lim <jnsg.lim0@gmail.com>
2023-07-19 13:48:28 -0700

    Created codewars50.js

---
+++ codewars50.js
@@ -0,0 +1,18 @@
+// 5 without numbers !!
+
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// P: no parameters going in since it'll always return 5  
+// R: 5 - cannot use num 5 
+// E: unusualFive() -> 5 
+//    unusualFive('something') -> 5 
+// P: 
+//    "fivee".length  
+//    "5" -> 5  
+//    return 5 
+
+function unusualFive() {
+  return 'fivee'.length
+}
