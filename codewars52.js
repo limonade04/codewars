abd2cef2c225c5e2d2e47724323124be407cfdb4
John Lim <jnsg.lim0@gmail.com>
2023-08-01 20:26:12 -0700

    Modified codewars52.js

--- codewars52.js
+++ codewars52.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-/*
-Stop gninnipS My sdroW!
-
-Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
-
-Examples:
-
-spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
-spinWords( "This is a test") => returns "This is a test" 
-spinWords( "This is another test" )=> returns "This is rehtona test"
-*/ 
-
-/* 
-P: str of word(s) 
-R: return same str with word reversed if letters >= 5 then reverse word 
-E: "Hey fellow warriors"  ->  "Hey wollef sroirraw" 
-   "This is a test"  ->  "This is a test" 
-P:  loop (str)
-    conditional ( word >= 5 letters ) ;  reverse word ; word 
-*/ 
-
-function spinWords(string){
-    return string.split('').forEach(word => word.length>= 5 ? word.reverse() : word )
-}
-=======
 /* Find the divisors!
 
 Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#, empty table in COBOL) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
@@ -51,4 +25,3 @@ function divisors(integer) {
     return prime.length===0 ? `${integer} is prime`: prime
 }
 
->>>>>>> 39f51df7bdeacd551f44895c96c0761aefe3447d
