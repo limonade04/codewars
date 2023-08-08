6247a1464c4d0db3de895ba3acd4e5609bf5444b
John Lim <jnsg.lim0@gmail.com>
2023-08-07 21:28:09 -0700

    Created codewars60.js

---
+++ codewars60.js
@@ -0,0 +1,15 @@
+/* 
+Sum Mixed Array
+
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+*/
+
+function sumMix(x){
+    return x.map( num => Number(num)).reduce((acc, cum) => acc+cum)
+}
+
+console.log(sumMix([9, 3, '7', '3'])) // 22
+console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]),) //  42
+console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0'])) //  41
