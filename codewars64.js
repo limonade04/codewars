5f4ba6f7f12ad113e87d3846bd45b6fedf12c6cd
John Lim <jnsg.lim0@gmail.com>
2023-08-15 19:59:40 -0700

    Modified codewars64.js

--- codewars64.js
+++ codewars64.js
@@ -31,3 +31,10 @@ const shortcut = string => {
     })
     return word
 }
+
+// function shortcut (string) {
+//     return string
+//       .split('')
+//       .filter(str => !'aeiou'.includes(str))
+//       .join('')
+//   }
