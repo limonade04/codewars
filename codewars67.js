e40816b9fd4cbc4a46c469141f257edca50ef6a2
John Lim <jnsg.lim0@gmail.com>
2023-08-19 11:42:04 -0700

    Modified codewars67.js

--- codewars67.js
+++ codewars67.js
@@ -62,6 +62,24 @@ function declareWinner(fighter1, fighter2, firstAttacker) {
 }
 
 
+/* 
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+  var fac1 = Math.ceil( fighter1.health / fighter2.damagePerAttack );
+  var fac2 = Math.ceil( fighter2.health / fighter1.damagePerAttack );
+  if(fac1 < fac2) {
+    return fighter2.name;
+  } else if(fac2 < fac1) {
+    return fighter1.name;
+  } else {
+    return firstAttacker;
+  }
+}
+ 
+*/
+
+
+
 console.log(declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew"))  // "Lew"
 console.log(declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Harry"))  // "Harry"
 console.log(declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harry"))  // "Harald"
