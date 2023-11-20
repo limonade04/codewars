33534f79376dfdffde95a905b829559bac259b66
John Lim <jnsg.lim0@gmail.com>
2023-11-20 10:02:23 -0800

    Created codewars95.js

---
+++ codewars95.js
@@ -0,0 +1,7 @@
+/* Function 2 - squaring an argument -- 8 kyu 
+Now you have to write a function that takes an argument and returns the square of it.
+*/ 
+
+function square(number){
+    return number*number
+} 
