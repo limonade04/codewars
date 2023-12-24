c09b209fa15b1cf6f27adb1e3094905ffd4bb490
John L <yujohn412@gmail.com>
2023-12-23 21:41:47 -0800

    Created codewars106.js

---
+++ codewars106.js
@@ -0,0 +1,26 @@
+/* Reverse Words -- 7 kyu
+Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+*/ 
+
+/*
+str - string parameter 
+return - string but in reverse AND all spaces in string should be retained. 
+
+str.toArray().split()
+*/
+
+
+function reverseWords(str) {
+    return str.split(' ').map(function(word){
+        return word.split('').reverse().join('');
+    }).join(' ');
+}
+
+console.log(reverseWords('The quick brown fox jumps over the lazy dog.'))  // 'ehT kciuq nworb xof spmuj revo eht yzal .god'
+console.log(reverseWords('apple')) //  'elppa'
+console.log(reverseWords('a b c d'))  //  'a b c d'
+console.log(reverseWords('double  spaced  words'))  // 'elbuod  decaps  sdrow'
